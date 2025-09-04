9a85fdd19d89e30842190f8c8e7cfee02f0a813b
Nicolas Chamorro <nichagiro@gmail.com>
2025-09-03 21:01:36 -0500

    update numbers pjone

--- src/db/general.ts
+++ src/db/general.ts
@@ -1,3 +1,3 @@
-export const WHATSAPP_NUMBER = "573001102276"
-export const CONTACT_NUMBER = "+57 300 110 22 76"
+export const WHATSAPP_NUMBER = "573188602676"
+export const CONTACT_NUMBER = "+57 318 860 2676"
 export const EMAIL = "info@mpmbtl.com"
